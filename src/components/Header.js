1b6332066b20404e3d43f813cbe335f1603f5c7e
divyanshu <divyanshu65493@gmail.com>
2025-02-09 23:14:33 +0530

    header part almost done

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FaSearch } from 'react-icons/fa'; // Import the search icon
+
+const Header = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">MyWebsite</div>
+      <div className="navbar__links">
+        <button className="navbar__button">Home</button>
+        <button className="navbar__button">Contact Us</button>
+        <button className="navbar__button">Volunteers</button>
+        <button className="navbar__button">Requests</button>
+      </div>
+      <div className="navbar__search">
+        <FaSearch className="navbar__search-icon" /> {/* Add the search icon */}
+        <input
+          type="text"
+          className="navbar__search-input"
+          placeholder="Search Medical Supplies"
+          aria-label="Search Medical Supplies"
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
